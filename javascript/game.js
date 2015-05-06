8c2450a39ddf64e25476d8733f260bbf1398d7a5
Hesham Massoud <heshamhamdymassoud@gmail.com>
2015-05-06 12:20:09 +0200

    + Add beer image. + Apply Constructor pattern. + Apply Prototype patten.

--- javascript/game.js
+++ javascript/game.js
@@ -5,36 +5,75 @@ canvas.width = 512;
 canvas.height = 480;
 document.body.appendChild(canvas);
 
-// Background image
-var bgReady = false;
-var bgImage = new Image();
-bgImage.onload = function () {
-	bgReady = true;
-};
-bgImage.src = "images/background.png";
 
-// Hero image
-var heroReady = false;
-var heroImage = new Image();
-heroImage.onload = function () {
-	heroReady = true;
-};
-heroImage.src = "images/hero.png";
+function GameObject(imageName){
+	this.image = new Image();
+	this.image.src = "images/" + imageName + ".png";
+	this.x = 0;
+	this.y = 0;
 
-// Monster image
-var monsterReady = false;
-var monsterImage = new Image();
-monsterImage.onload = function () {
-	monsterReady = true;
-};
-monsterImage.src = "images/monster.png";
+}
+var HeroSingleton = (function () {
+    var instance;
+ 
+    function createInstance() {
+        var object = new Hero("hero");
+        return object;
+    }
 
-// Game objects
-var hero = {
-	speed: 256 // movement in pixels per second
-};
-var monster = {};
-var monstersCaught = 0;
+ 
+    return {
+        getInstance: function () {
+            if (!instance) {
+                instance = createInstance();
+            }
+            console.log(instance);
+            return instance;
+        }
+    };
+})();
+ 
+
+
+function Hero (imageName) {
+	GameObject.call(this, imageName);
+	this.score = 0;
+	this.speed = 256;
+	this.x = canvas.width / 2;
+	this.y = canvas.height / 2;
+	this.reset = function() {
+		this.x = canvas.width / 2;
+		this.y = canvas.height / 2;
+	};
+}
+Hero.prototype = GameObject;
+
+function Beer (imageName){
+	GameObject.call(this, imageName);
+	this.isHealthy = true;
+	this.reset = function () {
+		this.x = 32 + (Math.random() * (canvas.width - 64));
+		this.y = 32 + (Math.random() * (canvas.width - 64));
+	};
+	this.render = function() {
+		ctx.drawImage(this.image, this.x, this.y);
+	}
+}
+Beer.prototype = GameObject;
+
+function Monster (imageName){
+	GameObject.call(this, imageName);
+	this.isHealthy = false;
+	this.reset = function () {
+		this.x = 32 + (Math.random() * (canvas.width - 64));
+		this.y = 32 + (Math.random() * (canvas.width - 64));
+	};
+	this.render = function() {
+		ctx.drawImage(this.image, this.x, this.y);
+	}
+}
+Monster.prototype = GameObject;
+
 
 // Handle keyboard controls
 var keysDown = {};
@@ -48,17 +87,20 @@ addEventListener("keyup", function (e) {
 }, false);
 
 // Reset the game when the player catches a monster
-var reset = function () {
-	hero.x = canvas.width / 2;
-	hero.y = canvas.height / 2;
+var reset = function (moveHero, objects) {
+	if(moveHero){
+		hero.reset();
+	}
 
-	// Throw the monster somewhere on the screen randomly
-	monster.x = 32 + (Math.random() * (canvas.width - 64));
-	monster.y = 32 + (Math.random() * (canvas.height - 64));
+	// Throw the gameObject somewhere on the screen randomly
+	for (var i = objects.length - 1; i >= 0; i--) {
+		objects[i].reset();
+	};
+
 };
 
 // Update game objects
-var update = function (modifier) {
+var update = function (modifier, objects) {
 	if (38 in keysDown) { // Player holding up
 		hero.y -= hero.speed * modifier;
 	}
@@ -73,51 +115,67 @@ var update = function (modifier) {
 	}
 
 	// Are they touching?
-	if (
-		hero.x <= (monster.x + 32)
-		&& monster.x <= (hero.x + 32)
-		&& hero.y <= (monster.y + 32)
-		&& monster.y <= (hero.y + 32)
-	) {
-		++monstersCaught;
-		reset();
-	}
+	for (var i = objects.length - 1; i >= 0; i--) {
+		if (
+		hero.x <= (objects[i].x + 32)
+		&& objects[i].x <= (hero.x + 32)
+		&& hero.y <= (objects[i].y + 32)
+		&& objects[i].y <= (hero.y + 32)
+		) {
+			if(objects[i].isHealthy){
+				++hero.score;
+				reset(false, objects);
+			}
+			else
+			{
+				hero.score = 0;
+				reset(true, objects);
+			}
+			break;
+		}
+	};
+
+
 };
 
 // Draw everything
-var render = function () {
-	if (bgReady) {
-		ctx.drawImage(bgImage, 0, 0);
-	}
+var render = function (objects) {
 
-	if (heroReady) {
-		ctx.drawImage(heroImage, hero.x, hero.y);
-	}
+	ctx.drawImage(bgImage.image, 0, 0);
+	ctx.drawImage(hero.image, hero.x, hero.y);
 
-	if (monsterReady) {
-		ctx.drawImage(monsterImage, monster.x, monster.y);
-	}
+	for (var i = objects.length - 1; i >= 0; i--) {
+		objects[i].render();
+	};
 
 	// Score
 	ctx.fillStyle = "rgb(250, 250, 250)";
 	ctx.font = "24px Helvetica";
 	ctx.textAlign = "left";
 	ctx.textBaseline = "top";
-	ctx.fillText("Goblins caught: " + monstersCaught, 32, 32);
+	ctx.fillText("Score: " + hero.score, 32, 32);
 };
 
 // The main game loop
 var main = function () {
+
 	var now = Date.now();
 	var delta = now - then;
 
-	update(delta / 1000);
-	render();
+	update(delta / 1000, objects);
+	render(objects);
 
+	refresh -= 200;
+	if(refresh == 0)
+	{
+		refresh = 10000;
+		reset(false, objects);
+	}
 	then = now;
-
-	// Request to do this again ASAP
 	requestAnimationFrame(main);
+	
+	// Request to do this again ASAP
+	
 };
 
 // Cross-browser support for requestAnimationFrame
@@ -125,6 +183,17 @@ var w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
 // Let's play this game!
+
+
+var beer = new Beer('beer');
+var monster = new Monster('monster');
+var hero = HeroSingleton.getInstance();
+var bgImage = new GameObject('background');
+var objects = [monster,beer];
+var refresh = 10000;
 var then = Date.now();
-reset();
+
+
+
+reset(true, objects);
 main();
